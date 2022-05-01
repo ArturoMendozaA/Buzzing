e3a16b23de6b480123fb57d1027a17c4d9b125c5
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-04-30 20:38:31 -0500

    modified the expected code to check for object inside the array

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -3,6 +3,8 @@ describe('Test for Reader', () => {
     test('Check if it returns a name contained in the json file', () => {
         const jsonFile = ('explorers-copy.json')
         const explorersObject = Reader.objectifying (jsonFile)
-        expect(explorersObject.explorers).toMatch('Woopa')
+        const expected = [{"githubUsername": "ajolonauta1", "mission": "node", "name": "Woopa1"}];
+        expect(Object.values(explorersObject)).not.toEqual(expect.arrayContaining(expected));
+        //.toMatch('Woopa')
     })
 })
