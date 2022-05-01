ca0dff529a1da5e1174dfd714a3ce4a9daabc16e
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-04-30 19:26:28 -0500

    Mdifies the path to json copy file

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -1,7 +1,7 @@
 const Reader = require('./../../../app/lib/utils/Reader.js')
 describe('Test for Reader', () => {
     test('Check if it returns a name contained in the json file', () => {
-        const jsonFile = ('explorers.json')
+        const jsonFile = ('explorers-copy.json')
         const explorersObject = Reader.objectifying(jsonFile)
         expect(explorersObject.explorers).toMatch('Woopa')
     })
