2a6f7007316eda0abaa0706191f056adb7182f97
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-04-30 19:10:42 -0500

    Adds the code for parsing the json file

---
+++ app/lib/utils/Reader.js
@@ -0,0 +1,8 @@
+const rawdata = fs.readFileSync("explorers.json");
+const explorers = JSON.parse(rawdata);
+class Reader{
+    static obectifying(jsonFile){
+        JSON.parse(jsonFile)
+        returns
+    }
+}
