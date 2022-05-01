24602ae11fef31c9820408ee7ef05a3d87abcdf2
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-01 14:59:02 -0500

    Declare Static methods

---
+++ app/lib/services/ExplorerService.js
@@ -0,0 +1,12 @@
+class ExplorerService{
+    static filterByMission(explorers, mission){
+
+    }
+    static getAmountOfExplorersByMission(explorers, mission){
+
+    }
+    static getExplorersUsernamesByMission(explorers, mission){
+        
+    }
+}
+module.exports = ExplorerService
