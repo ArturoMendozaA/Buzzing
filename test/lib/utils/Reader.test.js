e2ef05427fb0507e3b0d30443d29f1ad7d53dd69
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-04-30 19:09:45 -0500

    Add Reader test,

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require('./../../../app/lib/utils/Reader.js')
+describe('Test for Reader', () => {
+    test('Check if it returns a name contained in the json file', () => {
+        const jsonFile = ('explorers.json')
+        const explorersObject = Reader.objectifying(jsonFile)
+        expect(explorersObject.explorers).toMatch('Woopa')
+    })
+})
