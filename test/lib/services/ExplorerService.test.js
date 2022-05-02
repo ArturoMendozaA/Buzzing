e62310a90667c6f45a920f7b9cf30ce7c75a7dd8
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-01 22:01:17 -0500

    add test 3

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -1,15 +1,23 @@
 const ExplorerService = require('./../../../app/lib/services/ExplorerService')
 
 describe('Test for ExplorerService', () => {
-    test('filter by mission', () => {
+    test('1) filter by mission', () => {
         const explorers = ('explorers-copy.json')
-        const explorersInNode = ExplorerService.filterByMission(explorers,'node')
+        const mission = ('node')
+        const explorersInNode = ExplorerService.filterByMission(explorers,mission)
         expect(explorersInNode.length).toBe(10)
     })
-    test('Returns the number of explorers',() => {
+    test('2) Returns the number of explorers',() => {
         const explorers = ('explorers-copy.json')
-        const numExplorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, 'node')
+        const mission = ('node')
+        const numExplorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, mission)
         expect(numExplorersInNode).toBe(10)
-
+    })
+    test('3) Test if it returns an expected name', () => {
+        const explorers = ('explorers-copy.json')
+        const mission = ('node')
+        const nameExplorersInNode = ExplorerService.getExplorersUsernamesByMission(explorers, mission)
+        expect(nameExplorersInNode).toEqual(expect.objectContaining({"githubUsername": "ajolonauta1"}))
+        
     })
 })
