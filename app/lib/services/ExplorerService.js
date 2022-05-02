954224f8d4ab34d4b1ab3ef98dc874774ff59d4a
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-01 19:08:58 -0500

    testing not working

--- app/lib/services/ExplorerService.js
+++ app/lib/services/ExplorerService.js
@@ -1,12 +1,14 @@
+const Reader = require('./../utils/Reader')
 class ExplorerService{
     static filterByMission(explorers, mission){
-
+        const explorersInNode = Reader.readJsonFile.filter((explorer) => explorer.mission == "node");
+        return explorersInNode
     }
     static getAmountOfExplorersByMission(explorers, mission){
 
     }
     static getExplorersUsernamesByMission(explorers, mission){
-        
+
     }
 }
 module.exports = ExplorerService
--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -1,1 +1,10 @@
 const ExplorerService = require('./../../../app/lib/services/ExplorerService')
+const Reader = require('./../../../app/lib/utils/Reader')
+
+describe('Test for ExplorerService', () => {
+    test('filter by mission', () => {
+        const explorers = Reader.readJsonFile('explorers-copy.json')
+        const explorersInNode = ExplorerService.filterByMission(explorers,'node')
+        expect(explorersInNode.length).toBe(10)
+    })
+})
