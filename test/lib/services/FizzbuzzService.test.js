243e7c6cb18e166dd3acf1fdd0bd11da57c70dac
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-05 16:06:32 -0500

    Add fizzbuz test for score 1 and code

--- test/lib/services/FizzbuzzService.test.js
+++ test/lib/services/FizzbuzzService.test.js
@@ -2,7 +2,7 @@ const FizzbuzzService = require("../../../app/lib/services/FizzbuzzService")
 describe("test for Fizzbuzz Service", () => {
     test("Validacion score 1",() => {
         const explorer1 = {name: "Explorer1", score: 1}
-        FizzbuzzService.applyValidationInExplorer(explorer1)
+         FizzbuzzService.applyValidationInExplorer(explorer1)
         expect(explorer1.trick).toBe(1)
     })
 })
