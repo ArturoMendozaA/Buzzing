2989eff32f5915ad5d528fc1ef116e122edb18e9
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-07 12:25:52 -0500

    test 1 for ExplorerController

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -1,1 +1,9 @@
-const ExplorerController = require("")
+const ExplorerController = require("./../../../app/lib/controllers/ExplorerController")
+ describe("Test for the ExplorerController", () =>{
+     test("1) get list of explorers by mission", () =>{
+
+     const mission = ("node")
+     const explorersByMission = ExplorerController.ExplorerService(mission)
+     expect(explorersByMission.lenght).toBe(10)
+     })
+ })
