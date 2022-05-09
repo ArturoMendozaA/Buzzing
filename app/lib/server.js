2a3d7facf001f256a5dc6f63e6bfd187b5379807
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-09 16:31:49 -0500

    Add usernames node

--- app/lib/server.js
+++ app/lib/server.js
@@ -23,3 +23,8 @@ app.get("/v1/explorers/amount/:mission", (request, response) => {
     const explorersAmountInMission = ExplorerController.getExplorersAmountByMission(mission);
     response.json({mission: request.params.mission, quantity: explorersAmountInMission});
 })
+app.get("/v1/explorers/usernames/:mission", (request, response) => {
+    const mission = request.params.mission;
+    const exploreUsernames = ExplorerController.getExplorersUsernamesByMission(mission)
+    response.json({mission: request.params.mission, names:exploreUsernames})
+})
