26644c5786ae85ebe0d157af61796878c1b62da7
ArturoMendozaA <arturo_mendoza@outlook.com>
2022-05-09 16:05:13 -0500

    Adds explorers by mission, and explorers amount by mission

--- app/lib/server.js
+++ app/lib/server.js
@@ -1,9 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.get('/', (req, res) =>{
-    res.send('HelloWorld!')
-})
+const ExplorerController = require("./controllers/ExplorerController");
+const express = require("express");
+const { request } = require("express");
+const { response } = require("express");
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.get("/", (request, response) => {
+    response.json({message: "FizzBuzz Api welcome!"});
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`FizzBuzz API in localhost:${port}`);
+});
+app.get("/v1/explorers/:mission", (request, response) => {
+    const mission = request.params.mission;
+    const explorersInMission = ExplorerController.getExplorersByMission(mission);
+    response.json(explorersInMission);
+})
+app.get("/v1/explorers/amount/:mission", () => {
+    const mission = request.params.mission;
+    const explorersAmountInMission = ExplorerController.getExplorersAmountByMission(mission);
+    response.json({mission: request.params.mission, quantity: explorersAmountInMission})
 })
